4f318c1a3507aa122dc6a4f22f25b70d1038641f
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-02-22 11:17:25 +0600

    Lab 2 updated

--- Project/Registration.php
+++ Project/Registration.php
@@ -1,10 +1,104 @@
-<?php include "control/results.php"; ?>
+<!DOCTYPE html>
+    <head>
+        <title> Registration </title>
+    </head>
+    <body>
+        <h1 style="text-align: center">Registration</h1>
+        <form style="max-width: 1080px; margin: auto;" method="POST" name="Registration" action="<?php echo $_SERVER["PHP_SELF"] ?>">
+            <table>
+                <tr>
+                    <td>
+                        <label>Name:</label>
+                    </td>
+                    <td>
+                    <label>Name:</label>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Registration Form</title>
     </head>
     <body>
+        <?php  
+            $validateCheckBox = "";
+            $validateName = "";
+            $validateUserName = "";
+            $validateEmail = "";
+            $validateDob = "";
+            $validateGender = "";
+            $v1=$v2=$v3= "";
+            $validatePassword = "";
+            $validateCPassword = "";
+            $ValidateAllField = "";
+
+            if($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+                $name = $_REQUEST["name"];
+                $email = $_REQUEST["email"];
+                $userName = $_REQUEST["username"];
+                $password = $_REQUEST["password"];
+                $cpassword = $_REQUEST["cpassword"];
+
+                if(empty($name) || empty($_REQUEST["gender"]) || empty($password) || empty($cpassword) || empty($userName))
+                {
+                    $ValidateAllField = "Please Fillup All The Field!!";
+                }
+                if(empty($name) || strlen($name)<4)
+                {
+                    $validateName = "You must Enter a valid Name";
+                }
+                if(empty($userName) || strlen($userName)<6 || !preg_match("/^[A-Za-z0-9_~\-!@.#\$%\^&*\(\)]+$/",$userName))
+                {
+                    $validateUserName = "Please enter Username Correctly";
+                }
+                if(empty($email) || !preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i",$email))
+                {
+                    $validateEmail = "You must Enter a valid Email";
+                }
+                
+                if(empty($_REQUEST["gender"]))
+                {
+                    $validateGender = "Please select a gender";
+                }
+                if(empty($_REQUEST["dob"]))
+                {
+                    $validateDob = "Please Enter a Date";
+                }
+                
+                if(empty($password) || strlen($password)<8)
+                {
+                    $validatePassword = "You must Enter a Password";
+                }
+                if( $password !=  $cpassword)
+                {
+                    $validateCPassword = "Confirm Password isn't match";
+                }
+                if(!isset($_REQUEST["vehicle1"]) && !isset($_REQUEST["vehicle2"]) && !isset($_REQUEST["vehicle3"]))
+                {
+                    $validateCheckBox = "Please select at least one checkbox";
+                }
+            }
+
+        ?>
         <h1>Registration</h1>
         <form value="Registration" action="<?php echo $_SERVER["PHP_SELF"]?>" method="POST"> 
             <table>
@@ -122,3 +216,4 @@
         </form>
     </body>
 </html>
+*/
